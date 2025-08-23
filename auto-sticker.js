a3b19590eab8e231f2a2ef054d3abfc31bc55470
♰ĐɆɃɌØN♰ <alexdebron2006@gmail.com>
2025-08-23 15:34:43 +0530

    Add files via upload

---
+++ auto-sticker.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  on: 'body'
+}, async (conn, mek, m, { from, body }) => {
+  try {
+    const jsonUrl = 'https://raw.githubusercontent.com/alexdebron-mv/Mech-data/refs/heads/main/autosticker.json';
+    const res = await axios.get(jsonUrl);
+    const data = res.data;
+
+    for (const keyword in data) {
+      if (body.toLowerCase() === keyword.toLowerCase()) {
+        if (config.AUTO_STICKER === 'true') {
+          await conn.sendMessage(
+            from,
+            {
+              sticker: { url: data[keyword] },
+              package: 'MECH X4-MD'
+            },
+            { quoted: mek }
+          );
+        }
+      }
+    }
+  } catch (e) {
+    console.error('AutoSticker error:', e);
+  }
+});
